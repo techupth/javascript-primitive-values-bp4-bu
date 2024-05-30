18bebd01774efbd957cab69c8c5d38386a67240e
Bura <bu_bangplee@hotmail.com>
2024-05-30 17:10:44 +0700

    feat: submit work Assignment : JavaScript Primitive Values Assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
+let totalPrice = 27000;
+let disCount = totalPrice*25/100;
+let discountPrice = totalPrice-disCount;
+console.log(discountPrice);
 
 // Start coding here
